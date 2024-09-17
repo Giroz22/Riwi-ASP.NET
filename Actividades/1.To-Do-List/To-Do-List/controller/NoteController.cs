958126070e4abe9d088fdf2328c7e2ab7dd9b134
Alejandro Giraldo Serna <giraldosernaalejandro16@gmail.com>
2024-09-17 11:16:02 -0500

    All endpoint added

--- Actividades/1.To-Do-List/To-Do-List/controller/NoteController.cs
+++ Actividades/1.To-Do-List/To-Do-List/controller/NoteController.cs
@@ -16,8 +16,60 @@ public class NoteController : ControllerBase
 
     //Get Notes
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Note>>> Index()
+    public async Task<ActionResult<IEnumerable<Note>>> FindAll()
     {        
         return await _context.Notes.ToListAsync();
     }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Note>> FindById(int id)
+    {
+        var user = await _context.Notes.FindAsync(id);
+
+        if(user == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(user);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Note>> Create(Note note)
+    {
+        _context.Notes.Add(note);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction("FindById", new { id = note.Id }, note);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<Note>> Update(int id, [FromBody] Note newNote)
+    {
+        if(id != newNote.Id) return BadRequest();        
+
+        var note = await _context.Notes.FindAsync(id);
+        if(note == null) return NotFound();
+
+        _context.Entry(note).CurrentValues.SetValues(newNote);
+        await _context.SaveChangesAsync();
+
+        return Ok(note);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var note = await _context.Notes.FindAsync(id);
+        if(note == null)
+        {
+            return NotFound();
+        }
+
+        note.Deleted = true;
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
 }
--- Actividades/1.To-Do-List/To-Do-List/data/BaseContext.cs
+++ Actividades/1.To-Do-List/To-Do-List/data/BaseContext.cs
@@ -5,10 +5,16 @@ namespace ToDoApi.Data
 {
     public class BaseContext : DbContext
     {
-        public BaseContext(DbContextOptions<BaseContext> options) : base(options)
-        {
 
+         // We overwrite the OnModelCreating method to add the Soft Delete global filter
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Exclude Notes that are "deleted" (soft deleted)
+            modelBuilder.Entity<Note>().HasQueryFilter(p => !p.Deleted);
         }
+
+        public BaseContext(DbContextOptions<BaseContext> options) : base(options)
+        {}        
 
         //Our models
         public DbSet<Note> Notes {get; set;}
